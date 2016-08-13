677502ebf84ff4350f848024cc06f66132a69a26
Miguel Á. Domínguez Coloma <m@eridem.net>
2016-08-13 15:49:45 +0100

    Suites by argument.

--- patata-cli.js
+++ patata-cli.js
@@ -1,19 +1,23 @@
-"use strict";
+'use strict'
 
-var Liftoff = require('liftoff');
+const colors = require('colors')
+const yargs = require('yargs')
+    .help('h')
+    .alias('h', 'help')
+    .usage('Usage: patata <command> [options]')
+    .options(require('./lib/patata-cli.argv'))
+    
+const args = yargs.argv
 
-var Patata = new Liftoff({
-  name: 'patata',
-  processTitle: 'patata',
-  moduleName: 'patata',
-  configName: 'patatafile'
-});
-
-Patata.launch({}, function(result) {
-    // Require patatafile
-    require(result.configPath);
-    var patata = require(result.modulePath);
-
-    var cli = patata.cli;
-    cli(result, patata);
-});
+if (args.suite === '') {
+  // Show suites
+  console.log('[Option: Available suites]'.white.bold.underline)
+  require('./lib/show-suites')()
+} else if (args.suite && args.suite.length) {
+  // Run suite
+  console.log('[Option: Run suite]'.white.bold.underline)
+  require('./lib/run-suite')(args.suite);
+} else {
+  yargs.showHelp();
+  return;
+}
