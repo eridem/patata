9d7017024d2fb806dc3147d565d43171edce3efd
eridem <m@eridem.net>
2016-10-09 18:07:54 +0200

    Add tests for date-time

--- test/modules/date-time.js
+++ test/modules/date-time.js
@@ -1,14 +1,18 @@
-/* global before, describe */
+/* global describe, it */
 
 const { join } = require('path')
 const chai = require('chai')
 chai.should()
+const expect = chai.expect
 
 const file = 'date-time.js'
 const target = require(join(__dirname, '../../lib/modules/', file))
 
 describe(file, function () {
-  before(function () {
-    target({ })
+  describe('using .dateTimeAsString()', function () {
+    it('should return a date with yyyyMMdd_hhmmss format', function () {
+      const date = target().dateTimeAsString()
+      expect(date.match(/^\d\d\d\d\d\d\d\d_\d\d\d\d\d\d$/gi)).not.to.equal(null)
+    })
   })
 })
